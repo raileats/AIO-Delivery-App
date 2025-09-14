6923abf7ac40c52f785097f0fb3797da3ce1fc89
raileats <raileats@gmail.com>
2025-09-15 00:41:55 +0530

    Delete backend directory

--- backend/src/db/sqlite.js
+++
@@ -1,8 +0,0 @@
-import Database from 'better-sqlite3';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-export const db = new Database(path.join(__dirname, 'data.sqlite'));
--- backend/src/imapClient.js
+++
@@ -1,2 +0,0 @@
-// Placeholder for IMAP email fetcher
-// Here you would configure IMAP to read vendor emails and insert into DB
